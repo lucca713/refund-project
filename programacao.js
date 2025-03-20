98f48a0d753e1086bcc63460664bbd0a63855af8
Lucca <cerasommalucca@gmail.com>
2025-03-20 08:00:39 -0300

    Adicionando funcionalidade das moedas

--- programacao.js
+++ programacao.js
@@ -1,7 +1,30 @@
 //seleciona elementos do form
 const  amount = document.querySelector("#amount")
+const expense = document.querySelector("#expense")
+const category = document.querySelector("#category")
+const form = document.querySelector("form")
 
+//captura evento de input para formatar o valor
 amount.oninput = () =>{
-    amount.value = amount.value.replace(/\D/g ,"")
-    console.log(amount.value)
+    let valor = amount.value.replace(/\D/g,"")
+
+    valor = Number(valor)/100
+    
+    amount.value = formatCurrencyBRL(valor)
+}
+
+
+function formatCurrencyBRL(value){
+    value = value.toLocaleString("pt-BR",{
+        style:"currency",
+        currency:"BRL",
+    })
+
+    return value
+}
+
+
+form.onsubmit = (event) =>{
+    event.preventDefault()
+    
 }
